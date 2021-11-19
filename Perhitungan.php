4b251f0a0784e94600e97efa841d9e97e451815b
faateehrzl <muhammadfaatihrizalauliya@gmail.com>
2021-11-19 23:33:18 +0700

    Initial commit

---
+++ Perhitungan.php
@@ -0,0 +1,44 @@
+<?php
+
+    include "index.php";
+    echo "<br><div id='main_struk'><h1><b>NOTA</b></h1></div>";
+
+    class buah {
+        var $totalMangga, $totalJambu, $totalSalak;
+
+        public function __construct($mangga, $jambu, $salak){
+            $this->mangga = $mangga;
+            $this->jambu = $jambu;
+            $this->salak = $salak;
+        }
+
+        public function getMangga(){
+            $this->totalMangga = $this->mangga * 15000;
+            echo "<div id='struk'>Total Harga Mangga : Rp.$this->totalMangga</div>";
+        }
+            
+        public function getJambu(){
+            $this->totalJambu = $this->jambu * 13000;
+            echo "<div id='struk'>Total Harga Jambu : Rp.$this->totalJambu</div>";
+        }  
+
+        public function getSalak(){
+            $this->totalSalak = $this->salak * 10000;
+            echo "<div id='struk'>Total Harga Salak : Rp.$this->totalSalak</div>";
+        }
+
+        public function total(){
+            $total = $this->totalMangga + $this->totalJambu + $this->totalSalak;
+            echo "<div id='main_struk'>Total Harga = Rp.$total</div>";
+        }
+    }
+$mangga = $_POST["mangga"];
+$jambu = $_POST["jambu"];
+$salak = $_POST["salak"];
+$transaksi = new buah($mangga, $jambu, $salak);
+$transaksi->getMangga();
+$transaksi->getJambu();
+$transaksi->getSalak();
+$transaksi->total();
+        
+?>
